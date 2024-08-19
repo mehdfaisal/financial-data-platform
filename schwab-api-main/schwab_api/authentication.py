472ebe3fab153219280a9eee117e0f709ab9b619
mehdfaisal <faisalu279@gmail.com>
2024-08-19 10:36:45 +0100

    Delete schwab-api-main directory

--- schwab-api-main/schwab_api/authentication.py
+++
@@ -1,9 +0,0 @@
-# authentication.py
-
-from schwab_api import urls  # Use absolute import
-
-def main():
-    urls.some_function()
-
-if __name__ == "__main__":
-    main()
--- schwab-api-main/schwab_api/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-
-def some_function():
-    print("Function from urls.py")
-
-class SomeClass:
-    def __init__(self):
-        print("SomeClass instance created")
